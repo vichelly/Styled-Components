832665eed30d600031a37615536dfc0c51820b67
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2022-11-22 11:18:07 -0300

    att

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,19 +1,42 @@
 import React from "react";
+import styled from"styled-components"
 import bank_logo from "../../assets/images/bank_logo.svg";
+import {corPrimaria} from '../UI/variaveis'
+
+const StyledHeader = styled.nav`
+  background-color: ${corPrimaria};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`
+
+const BtnCabecalho = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  color: ${(props) => props.primary ? corPrimaria : "white"};
+  background: ${(props) => props.primary ? "white" : corPrimaria};
+`
 
 const Cabecalho = () => {
   return (
-    <div className="cabecalho">
+    <StyledHeader>
       <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <BtnCabecalho primary href="https://google.com">
           Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </BtnCabecalho>
+        <BtnCabecalho href="https://google.com">
           Sair
-        </a>
+        </BtnCabecalho>
       </div>
-    </div>
+    </StyledHeader>
   );
 };
 
